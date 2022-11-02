308483e85f8e1153c2d1391669939ffb38b2acb9
madbull12 <huangandrian02@gmail.com>
2022-11-02 23:37:53 +0700

    cart item little changes

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { CartItem } from "../../lib/zustand";
+import { CartItem, useCartItem } from "../../lib/zustand";
+import { BsFillTrashFill } from "react-icons/bs";
+import useHover from "../../hooks/useHover";
 
 interface IProps {
   item: CartItem;
 }
 const CartItem = ({ item }: IProps) => {
+  const [hoverRef, isHovering] = useHover<HTMLDivElement>();
+  const { removeCartItem } = useCartItem();
+  const deleteCart = async()=>{
+    await removeCartItem(item.id)
+  }
   return (
     <motion.div
+      ref={hoverRef}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      className="flex items-center gap-x-4 "
+      exit={{ opacity:0 }}
+      className="flex w-full items-center gap-x-4 cursor-pointer"
     >
       <Image
         className="rounded-lg"
@@ -20,9 +29,14 @@ const CartItem = ({ item }: IProps) => {
         width={50}
         height={50}
       />
-      <div className="text-sm text-gray-400 ">
+      <div className="flex flex-col relative text-sm text-gray-400">
         <p>{item.name}</p>
         <p>${item.price}</p>
+        {isHovering && (
+          <div className="absolute bottom-0 right-0 ">
+            <BsFillTrashFill onClick={deleteCart} />
+          </div>
+        )}
       </div>
     </motion.div>
   );
