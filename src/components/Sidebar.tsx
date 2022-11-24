ad276df36c302c23513bcfc4c24f510f1df122ff
madbull12 <huangandrian02@gmail.com>
2022-11-24 21:18:29 +0700

    changes on router

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { IGameResp, IGenre } from "../../interface";
 import fetchData from "../../rawg/fetchData";
 import { motion } from "framer-motion";
-import { v4 } from 'uuid'
+import { v4 } from "uuid";
 
 import {
   RiGlobalLine,
@@ -19,34 +19,41 @@ import {
 } from "react-icons/ri";
 import { SiIos, SiLinux, SiNintendoswitch } from "react-icons/si";
 import { platform } from "os";
+import { useRouter } from "next/router";
 const Sidebar = () => {
-  const platformIcons = [{
-    id:4,
-    name:"PC",
-    icon:<RiWindowsFill />
-  },{
-    id:18,
-    name:"Playstation 4",
-    icon:<RiPlaystationFill />
-  },{
-    id:1,
-    name:"Xbox One",
-    icon: <RiXboxFill />
-  },{
-    id:7,
-    name:"Nintendo Switch",
-    icon: <SiNintendoswitch />
-  },
- {
-    id:3,
-    name:"IOS",
-    icon: <SiIos />
-  },
-  {
-    id:21,
-    name:"Android",
-    icon: <RiAndroidFill />
-  }]
+  const router = useRouter();
+  const platformIcons = [
+    {
+      id: 4,
+      name: "PC",
+      icon: <RiWindowsFill />,
+    },
+    {
+      id: 18,
+      name: "Playstation 4",
+      icon: <RiPlaystationFill />,
+    },
+    {
+      id: 1,
+      name: "Xbox One",
+      icon: <RiXboxFill />,
+    },
+    {
+      id: 7,
+      name: "Nintendo Switch",
+      icon: <SiNintendoswitch />,
+    },
+    {
+      id: 3,
+      name: "IOS",
+      icon: <SiIos />,
+    },
+    {
+      id: 21,
+      name: "Android",
+      icon: <RiAndroidFill />,
+    },
+  ];
 
   const {
     data: genres,
@@ -66,7 +73,7 @@ const Sidebar = () => {
   const [showAll, setShowAll] = useState(false);
 
   return (
-    <aside className="fixed min-h-screen z-50 left-0 top-0 w-56 bg-secondary p-4 ">
+    <aside className="fixed left-0 top-0 z-50 min-h-screen w-56 bg-secondary p-4 ">
       <div className="flex items-center justify-between text-white">
         <Link href="/">
           <p className="cursor-pointer text-2xl  font-black ">NXTGAME.</p>
@@ -85,26 +92,36 @@ const Sidebar = () => {
           {genres?.results
             .slice(0, showAll ? genres.results.length : 3)
             .map((genre) => (
-              <li className="cursor-pointer text-sm text-gray-400 px-2" key={v4()}>
-                <Link href={`/games?genres=${genre.slug}`}>
-                  <motion.div
-                    whileHover={{
-                      scale: 1.05,
-                    }}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    className="flex items-center gap-x-2 text-white"
-                  >
-                    <Image
-                      src={genre.image_background}
-                      width={40}
-                      height={40}
-                      objectFit="cover"
-                      className="rounded-lg"
-                    />
-                    <p>{genre.name}</p>
-                  </motion.div>
-                </Link>
+              <li
+                className="cursor-pointer px-2 text-sm text-gray-400"
+                key={v4()}
+                onClick={() =>
+                  router.push({
+                    pathname: "/games",
+                    query: {
+                      ...router.query,
+                      genres: genre.slug,
+                    },
+                  })
+                }
+              >
+                <motion.div
+                  whileHover={{
+                    scale: 1.05,
+                  }}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  className="flex items-center gap-x-2 text-white"
+                >
+                  <Image
+                    src={genre.image_background}
+                    width={40}
+                    height={40}
+                    objectFit="cover"
+                    className="rounded-lg"
+                  />
+                  <p>{genre.name}</p>
+                </motion.div>
               </li>
             ))}
           <button
@@ -122,22 +139,27 @@ const Sidebar = () => {
           </button>
         </div>
         <li className="text-2xl font-semibold text-white">
-          <Link href={`/platforms`}>
-                Platforms
-          </Link>
+          <Link href={`/platforms`}>Platforms</Link>
         </li>
         <div className="space-y-2">
-          {platformIcons.map((platform)=>(
-            <li className="cursor-pointer text-white text-lg" key={v4()}>
-              <Link href={`/games?platform=${platform.id}`}>
-                <div className="flex items-center gap-x-2">
-                  <span className="text-2xl">
-                  {platform.icon}
-
-                  </span>
-                  <p >{platform.name}</p>
-                </div>
-              </Link>
+          {platformIcons.map((platform) => (
+            <li
+              className="cursor-pointer text-lg text-white"
+              key={v4()}
+              onClick={() =>
+                router.push({
+                  pathname: "/games",
+                  query: {
+                    ...router.query,
+                    platform: platform.id,
+                  },
+                })
+              }
+            >
+              <div className="flex items-center gap-x-2">
+                <span className="text-2xl">{platform.icon}</span>
+                <p>{platform.name}</p>
+              </div>
             </li>
           ))}
         </div>
