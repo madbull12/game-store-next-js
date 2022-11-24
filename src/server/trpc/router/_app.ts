b01c5e507bfee26cc78e820365f223d4673f49d6
madbull12 <huangandrian02@gmail.com>
2022-11-24 17:26:34 +0700

    bug fixed

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,12 +1,10 @@
 // src/server/trpc/router/_app.ts
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
 import { cartRouter } from "./cartRouter";
 import { wishListRouter } from "./wishlist";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   cart:cartRouter,
   wishlist:wishListRouter
