cb2b11093182fb6b4f46fc24f15e99a864d14d2d
madbull12 <huangandrian02@gmail.com>
2022-12-21 00:07:38 +0700

    little changes

--- src/components/WishlistCard.tsx
+++ src/components/WishlistCard.tsx
@@ -1,9 +1,12 @@
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
+import { toast } from "react-hot-toast";
 import { IWishlist } from "../../interface";
 import { trpc } from "../utils/trpc";
 
 const WishlistCard = ({ wishlist }: { wishlist: IWishlist }) => {
+  const { status } = useSession()
     const utils = trpc.useContext()
   const { mutate: addCart } = trpc.cart.addCart.useMutation({
     onSuccess: () => {
@@ -16,6 +19,22 @@ const WishlistCard = ({ wishlist }: { wishlist: IWishlist }) => {
         utils.wishlist.getUserWishlists.invalidate();
       },
     });
+    const removeWishlist = async (e: React.SyntheticEvent) => {
+      e.stopPropagation();
+  
+      if (status === "unauthenticated") {
+        toast.error("You have to be logged in first!");
+        return;
+      }
+      
+  
+      toast.success(`Removed ${wishlist.name} from wishlist`);
+      await deleteFromWishlist({ gameId: wishlist.gameId });
+    };
+
+  
+      // await addCartItem(cart);
+
 
   return (
     <div className="neon flex items-center gap-y-4 gap-x-4 rounded-lg bg-primary flex-col md:flex-row p-4 text-white">
@@ -33,12 +52,10 @@ const WishlistCard = ({ wishlist }: { wishlist: IWishlist }) => {
       <div className=" md:ml-auto flex flex-col  justify-between">
         <p className="justify-self-end text-white text-center">$ {wishlist.price}</p>
         <div className="flex items-center gap-x-2 flex-col space-y-2 md:flex-row justify-center">
-          <button className="whitespace-nowrap rounded-lg bg-secondary px-4 py-2 font-bold text-white">
+          <button onClick={removeWishlist} className="whitespace-nowrap rounded-lg bg-secondary px-4 py-2 font-bold text-white">
             Remove
           </button>
-          <button className="whitespace-nowrap rounded-lg bg-secondary px-4 py-2 font-bold text-white">
-            Add to cart
-          </button>
+        
         </div>
       </div>
     </div>
